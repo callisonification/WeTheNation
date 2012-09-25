165d93a243bfd0378c5cf389ff8cf3b51806ee21
Chris Allison <callisonification@gmail.com>
2012-09-25 00:21:24 -0400

    curl test
    testing code
    
    Signed-off-by: Chris Allison <callisonification@gmail.com>

--- public/curl/index.php
+++ public/curl/index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo 'curl!';
+$ch = curl_init("http://www.govtrack.us/api/v1/bill");
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result
