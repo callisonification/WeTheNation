8a1721660e7e0fe872d39e9d0f04bd6fde192b49
Chris Allison <callisonification@gmail.com>
2012-09-25 00:25:41 -0400

    curl test
    code test 2
    
    Signed-off-by: Chris Allison <callisonification@gmail.com>

--- public/curl/index.php
+++ public/curl/index.php
@@ -1,10 +1,15 @@
 <?php
+// create a new cURL resource
+$ch = curl_init();
 
-$ch = curl_init("http://www.govtrack.us/api/v1/bill");
-
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, "http://www.govtrack.us/api/v1/bill/?congress=112&order_by=current_status_date");
 curl_setopt($ch, CURLOPT_HEADER, 0);
 
+// grab URL and pass it to the browser
 $result = curl_exec($ch);
+
+// close cURL resource, and free up system resources
 curl_close($ch);
 
-echo $result
+print $result;
