76ad1070477d4f5597fef2d67744e19309cbe728
Chris Allison <callisonification@gmail.com>
2012-11-01 04:28:37 -0400

    updated db config settings
    Signed-off-by: Chris Allison <callisonification@gmail.com>

--- public/alpha/application/config/database.php
+++ public/alpha/application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = 'wtn';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'root';
 $db['default']['database'] = 'WeTheNation';
