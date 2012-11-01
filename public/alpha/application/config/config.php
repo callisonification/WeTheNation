3ba661da216017d51472812ffbea04f445a8447f
Chris Allison <callisonification@gmail.com>
2012-11-01 17:12:28 -0400

    changed config.php
    Signed-off-by: Chris Allison <callisonification@gmail.com>

--- public/alpha/application/config/config.php
+++ public/alpha/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://www.wethenation.us/';
+$config['base_url']	= 'http://www.wethenation.us/alpha';
 
 /*
 |--------------------------------------------------------------------------
