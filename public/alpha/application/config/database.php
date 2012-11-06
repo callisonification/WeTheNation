11149de3c592a3f092cdfd7c1e6ba2caf2bfd097
Chris Allison <callisonification@gmail.com>
2012-11-05 19:58:01 -0500

    oops
    fixed a break
    
    Signed-off-by: Chris Allison <callisonification@gmail.com>

--- public/alpha/application/config/database.php
+++ public/alpha/application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
+$db['default']['password'] = 'Xnyw1Y58m6g1496';
 $db['default']['database'] = 'WeTheNation';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
